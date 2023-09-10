3dd74f8f0544fe7b5f0abc1a78717d40ca2e9ff0
Ghithrif Aprillio Rakhman <ggiffi03@gmail,com>
2023-09-10 22:44:20 +0700

    first commit

---
+++ dilaundry-backend/database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'username' => 'giffi',
+                'email' => 'ggiffi03@gmail.com',
+                'password' => Hash::make('1234567890'),
+            ],
+        ];
+        foreach ($users as $userItem) {
+            User::create($userItem);
+        }
+    }
+}
